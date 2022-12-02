57634c8f25ee7cecea9749f04ad5b56de8f74546
rakeshyadavj3 <71177481+rakeshyadavj3@users.noreply.github.com>
2022-12-02 13:02:30 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,11 +1,11 @@
-/* terraform {
+terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 4.0"
     }
   }
-} */
+}
 provider "aws" {
   region = "ap-south-1"
 }
