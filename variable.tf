43ec91dd726fe96e41348af9e39c86e83ec7119a
rakeshyadavj3 <71177481+rakeshyadavj3@users.noreply.github.com>
2022-12-02 13:17:23 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,5 +1,13 @@
 variable "cidr" {}
-variable "envname" {}
-variable "pubsubnet" {}
-variable "privatesubnet" {}
-variable "datasubnet" {}
+variable "envname" {
+  type = string
+  default = "cg-assignment"
+}
+variable "pubsubnet" {
+  type = string
+  default = "10.1.0.0/24"
+}
+variable "privatesubnet" {
+  type = string
+  default = "10.1.3.0/24"
+}
