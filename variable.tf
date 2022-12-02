6b4a5f3a21fdf3754c13e7d26e3b6e6608a0264f
rakeshyadavj3 <71177481+rakeshyadavj3@users.noreply.github.com>
2022-12-02 13:09:18 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,10 +1,5 @@
 variable "cidr" {}
 variable "envname" {}
-variable "region" {}
 variable "pubsubnet" {}
 variable "privatesubnet" {}
 variable "datasubnet" {}
-variable "azs" {}
-variable "ami" {}
-variable "instance_type" {}
-/* variable "pem" {} */
